b2b1f445348561a3cc16a0cdf82e3f9ca79013f9
Zeronetsec <senxhunter996@gmail.com>
2026-07-25 20:11:13 +0700

    add: .gitaction/replacer.py
    add: .github/workflows/replacer.yaml
    
    modified: .github/workflows/snake.yaml
    
    modified: .gitignore
    
    modified: README.md

---
+++ .gitaction/replacer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# https://github.com/Zeronetsec/Zeronetsec
+
+import os
+
+script_dir = os.path.dirname(
+    os.path.abspath(__file__),
+)
+
+readme_path = os.path.join(
+    script_dir, "..", "README.md",
+)
+
+old_url = "https://raw.githubusercontent.com/Zeronetsec/Zeronetsec/main/.gitaction/github-snake-tokyonight.svg"
+new_path = ".gitaction/github-snake-tokyonight.svg"
+
+try:
+    with open(readme_path, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    if old_url in content:
+        updated_content = content.replace(
+            old_url, new_path,
+        )
+
+        with open(
+            readme_path, "w", encoding="utf-8",
+        ) as f:
+            f.write(updated_content)
+        print(
+            f"[+] Changed: {old_url} -> {new_path}",
+        )
+    else:
+        print(
+            f"[!] Target URL: {old_url} not found!",
+        )
+
+except FileNotFoundError:
+    print(
+        f"[!] File: {os.path.abspath(readme_path)} not found!",
+    )
+
+# Copyright (c) 2026 Zeronetsec
