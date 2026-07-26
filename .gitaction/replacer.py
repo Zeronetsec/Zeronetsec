6f7a871f2ac85995d0b9056a386e293eab6f9c1c
Zeronetsec <senxhunter996@gmail.com>
2026-07-26 07:33:49 +0700

    modified: .gitaction/replacer.py
    modified: .github/workflows/replacer.yaml
    
    modified: .github/workflows/snake.yaml
    
    modified: README.md

--- .gitaction/replacer.py
+++ .gitaction/replacer.py
@@ -3,13 +3,9 @@
 
 import os
 
-script_dir = os.path.dirname(
-    os.path.abspath(__file__),
-)
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-readme_path = os.path.join(
-    script_dir, "..", "README.md",
-)
+readme_path = os.path.join(script_dir, "..", "README.md")
 
 old_url = "https://raw.githubusercontent.com/Zeronetsec/Zeronetsec/main/.gitaction/github-snake-tokyonight.svg"
 new_path = ".gitaction/github-snake-tokyonight.svg"
@@ -17,27 +13,17 @@ new_path = ".gitaction/github-snake-tokyonight.svg"
 try:
     with open(readme_path, "r", encoding="utf-8") as f:
         content = f.read()
-
     if old_url in content:
         updated_content = content.replace(
             old_url, new_path,
         )
-
-        with open(
-            readme_path, "w", encoding="utf-8",
-        ) as f:
+        with open(readme_path, "w", encoding="utf-8") as f:
             f.write(updated_content)
-        print(
-            f"[+] Changed: {old_url} -> {new_path}",
-        )
+        print(f"[+] Changed: {old_url} -> {new_path}")
     else:
-        print(
-            f"[!] Target URL: {old_url} not found!",
-        )
+        print(f"[!] Target URL: {old_url} not found!")
 
 except FileNotFoundError:
-    print(
-        f"[!] File: {os.path.abspath(readme_path)} not found!",
-    )
+    print(f"[!] File: {os.path.abspath(readme_path)} not found!")
 
 # Copyright (c) 2026 Zeronetsec
