581b8eb64f66de5b86371ed744afdc1a4e7ca699
Zeronetsec <senxhunter996@gmail.com>
2026-07-28 04:39:34 +0700

    add: .laction/execute/reqgen.py
    modified: .gitattributes
    
    modified: README.md
    
    remove: .gitaction/reqgen.py
    
    remove: .github/workflows/paramgen.yaml
    
    remove: .github/workflows/replacer.yaml
    
    remove: .github/workflows/reqgen.yaml
    
    rename: .gitaction/paramgen.py -> .laction/execute/paramgen.py
    
    rename: .gitaction/replacer.py -> .laction/execute/replacer.py
    
    rename: .gitaction/uiflag_params.flg -> .laction/output/uiflag_params.flg

--- .laction/execute/replacer.py
+++ .laction/execute/replacer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-readme_path = os.path.join(script_dir, "..", "README.md")
+ROOT_DIR = Path(__file__).resolve().parent
+project_root = ROOT_DIR.parent.parent
+
+readme_path = project_root / "README.md"
 
 old_url = "https://raw.githubusercontent.com/Zeronetsec/Zeronetsec/main/.gitaction/github-snake-tokyonight.svg"
 new_path = ".gitaction/github-snake-tokyonight.svg"
@@ -17,9 +20,10 @@ try:
         )
         with open(readme_path, "w", encoding="utf-8") as f:
             f.write(updated_content)
-        print(f"[+] Changed: {old_url} -> {new_path}")
+        print(f"\x1b[0;32m[+] \x1b[0mChanged: \x1b[0;32m{old_url} \x1b[1;90m-> \x1b[0;32m{new_path}\x1b[0m")
     else:
-        print(f"[!] Target URL: {old_url} not found!")
+        print(f"\x1b[0;33m[!] \x1b[0mTarget URL: \x1b[0;32m{old_url} \x1b[0mnot found!")
 
 except FileNotFoundError:
-    print(f"[!] File: {os.path.abspath(readme_path)} not found!")
+    print(f"\x1b[1;31m[!] \x1b[0mFile: \x1b[0;32m{os.path.abspath(readme_path)} \x1b[0mnot found!")
+    sys.exit(1)
---
+++ .laction/execute/reqgen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent
+project_root = ROOT_DIR.parent.parent
+
+input_file = project_root / ".install" / "extern" / "python_packages.txt"
+output_file = project_root / "requirements.txt"
+
+try:
+    content = input_file.read_text(encoding="utf-8")
+    output_file.write_text(content, encoding="utf-8")
+    print(f"\x1b[0;32m[+] \x1b[0mGenerated: \x1b[0;32m{output_file}\x1b[0m")
+    sys.exit(0)
+
+except FileNotFoundError:
+    print(f"\x1b[1;31m[!] \x1b[0mFile: \x1b[0;32m{input_file} \x1b[0mnot found!")
+    sys.exit(1)
+
+except Exception as e:
+    print(f"\x1b[1;31m[!] \x1b[0mError: \x1b[0;32m{e}\x1b[0m")
+    sys.exit(1)
